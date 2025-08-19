fa29ab3f5a37f8becef4246e687aea52b179912e
Pamela <pamela@cedros.dev>
2025-08-19 20:10:06 -0300

    qa test suite ok

--- src/QA/fileExplorerTests.js
+++ src/QA/fileExplorerTests.js
@@ -1,7 +1,14 @@
 import { getHome } from "../model/fileExplorerCore.js";
 
+/**
+ *
+ * @returns {Promise.<{pass:boolean,error:any}>}
+ */
 export const getHomeTest = async () => {
   let res = await getHome();
-  console.log(res);
-  return true;
+  let testPass = false;
+  if (res.success && typeof res.data[0] && !res.error) {
+    testPass = true;
+  }
+  return { pass: testPass, error: res.error };
 };
--- src/QA/summary.js
+++ src/QA/summary.js
@@ -8,15 +8,26 @@ const rl = readline.createInterface({
 class Tests {
   constructor() {
     this.menu();
+    this.total = 0;
+    this.pass = 0;
+    this.fail = 0;
   }
   menu() {
     console.log("MENU");
-    console.log("1 - file explorer tests");
+    console.log("0  -  Comprehensive Test");
+    console.log("1  -  File explorer tests");
 
-    rl.question("Select an option: ", answer => {
-      console.log({ answer });
+    rl.question(`\n\nWrite option: `, answer => {
       if (answer === "1") {
         this.fileExplorer();
+      }
+
+      switch (answer) {
+        case "0":
+          break;
+        case "1":
+          this.fileExplorer();
+          break;
       }
     });
   }
@@ -24,11 +35,46 @@ class Tests {
     console.clear();
     console.log("MENU");
     console.log("1 - getHome");
-    rl.question("Select an option: ", answer => {
-      if (answer === "1") {
-        getHomeTest();
+    rl.question("Select an option: ", async answer => {
+      switch (answer) {
+        case "1":
+          let res = await getHomeTest();
+          this.showResult(res.pass, "getHomeTest", res.error);
+          this.final();
+          break;
       }
     });
+  }
+
+  /**
+   * @param {boolean} pass
+   * @param {string} testName
+   * @param {string} [message]
+   */
+  showResult(pass, testName, message) {
+    this.total++;
+    if (pass) {
+      this.pass++;
+      console.log(
+        "\x1b[5m\x1b[42m PASS \x1b[0m " + `[${testName}] ${message ?? ""}`
+      );
+    } else {
+      this.fail++;
+      console.log(
+        "\x1b[5m\x1b[41m FAIL \x1b[0m " + `[${testName}] ${message ?? ""}`
+      );
+    }
+  }
+
+  final() {
+    console.log("Final Results");
+    console.log(`Total tests: ${this.total}`);
+    console.log(`Passed: ${this.pass}`);
+    console.log(`Failed: ${this.fail}`);
+    console.log(
+      `Success rate: ${this.total > 0 ? Math.round((this.pass / this.total) * 100) : 0}%`
+    );
+    rl.close();
   }
 }
 
